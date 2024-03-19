5619b26f422a2ea55cbde1658324d97d7c245010
sonnnayyyf <122763787+sonnnayyyf@users.noreply.github.com>
2024-03-19 11:40:29 +1100

    creating menu

--- vacuumClean.cpp
+++ vacuumClean.cpp
@@ -2,19 +2,57 @@
 #include "helper.h"
 
 using std::string;
+using namespace std;
 
-void showStudentInformation(string name, string id, string email){
-    // TODO
-}
+void displayMainMenu() {
+    cout << "Welcome to Vacuum Cleaning Game!" << endl;
+    cout << "-----------------------------" << endl;
+    cout << "1. Play Game" << endl;
+    cout << "2. Show student information" << endl;
+    cout << "3. Quit" << endl;
+    cout << "Please enter your choice: ";
+};
+void displayStudentInfo() {
+    cout << "-----------------------------------" << endl;
+    cout << "Name: Tran Hoang Son" << endl;
+    cout << "Student ID: s3978450" << endl;
+    cout << "Email: s3978450@rmit.edu.vn" << endl;
+    cout << "-----------------------------------" << endl;
+};
 
 int main()
 {
-    /**
-     * TODO: here's the main function. You can write the "main menu" loop/code
-     * here or you can make separate functions - up to you.
-     */
+    int choice;
 
-    std::cout << "Good bye!\n\n";
+    // Main menu loop:
+
+    while (true) {
+        displayMainMenu();
+        std::cin >> choice;
+        
+        switch (choice) {
+            case 1:
+                
+            case 2:
+                displayStudentInfo();
+                while (true) {
+                    cout << "Please enter 0 to return to the main menu: ";
+                    std::cin >> choice;
+                    if (choice == 0) // Exit student info display
+                        break;
+                    else
+                        std::cout << "Invalid choice. Please enter 0 to return to the main menu." << std::endl;
+                }
+                break;
+            case 3:
+                std::cout << "Exiting..." << std::endl;
+                return 0;
+            default:
+                std::cout << "Invalid choice. Please try again." << std::endl;
+                
+                break;
+        }
+    }
 
     return EXIT_SUCCESS;
 }
